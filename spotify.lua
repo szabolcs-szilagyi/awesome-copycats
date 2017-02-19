c4ba8cb821384102fa81a6a4c4f3ada79e1f4569
Szabolcs Szilagyi <szabolcs.szilagyi@planorama.com>
2017-02-19 20:40:51 +0100

    modular music backend: spotify down to bare minimum

--- spotify.lua
+++ spotify.lua
@@ -5,22 +5,10 @@ local dbus = dbus
 
 local spotify = {}
 
-spotify.player_status = {}
 spotify.widget = wibox.widget.textbox("")
 spotify.widget:set_font('Play 7')
 
 spotify.init = function ()
-  local controlActions = {
-    "play",
-    "pause",
-    "prev",
-    "next",
-  }
-
-  for i, action in ipairs(controlActions) do
-    spotify[action] = function () return spotify.action(action) end
-  end
-
   dbus.add_match("session", "path='/org/mpris/MediaPlayer2',interface='org.freedesktop.DBus.Properties',member='PropertiesChanged'")
   dbus.connect_signal(
     "org.freedesktop.DBus.Properties",
@@ -28,67 +16,18 @@ spotify.init = function ()
   )
 end
 
-local get_current = function (cb)
-  local noisy = [[bash -c '
-    sp current-oneline
-    ']]
-  awful.spawn.with_line_callback(noisy, {
-                                   stdout = cb,
-                                   stderr = function(line)
-                                     naughty.notify { text = "ERR:"..line}
-                                   end,
-  })
-end
-
-spotify.update = function (path, bus_path, payload, ...)
-  -- payload.PlaybackStatus
-  -- payload.Metadata
-  -- 
-  local json = require('json')
-  local args = {...}
-  naughty.notify { text = json.encode(payload) }
-  get_current (function (current)
-   spotify.widget:set_text(current)
-  end)
-end
+spotify.play = function () return spotify.action("play") end
+spotify.pause = function () return spotify.action("pause") end
+spotify.prev = function () return spotify.action("prev") end
+spotify.next = function () return spotify.action("next") end
 
 spotify.action = function (action)
   os.execute('sp ' .. action)
 end
 
-function spotify.post_update(result_string, parse_status_callback)
-  spotify.player_status = {}
-  local state = nil
-  if result_string:match("Playing") then
-    state = 'play'
-  elseif result_string:match("Paused") then
-    state = 'pause'
-  end
-  spotify.player_status.state = state
-  if state == 'play' or state == 'pause' then
-    awful.spawn.easy_async(
-      dbus_cmd .. "Metadata",
-      function(str) spotify.parse_metadata(str, parse_status_callback) end
-    )
-  else
-    parse_status_callback(spotify.player_status)
-  end
-end
-
-function spotify.parse_metadata(result_string, parse_status_callback)
-  h_table.merge(spotify.player_status, parse.find_values_in_string(
-    result_string,
-    "([%w]+): (.*)$",
-    { artist='artist',
-      title='title',
-      album='album',
-      date='contentCreated',
-      cover_url='artUrl'
-    }
-  ))
-  spotify.player_status.date = h_string.max_length(spotify.player_status.date, 4)
-  spotify.player_status.file = 'spotify stream'
-  parse_status_callback(spotify.player_status)
+spotify.update = function (path, bus_path, payload)
+  local meta = payload.Metadata
+  spotify.widget:set_text(meta['xesam:artist'][1] .. ' | ' .. meta['xesam:title'])
 end
 
 return spotify
